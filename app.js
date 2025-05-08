fbab68370eef2cb55cfc80cce68d07708b67910f
eglezg <ernestoglezg9182@hotmail.com>
2025-05-07 20:23:46 -0600

    Agregando frontend básico

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const usuariosRoutes = require('./routes/usuarios');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use('/usuarios', usuariosRoutes);
 
